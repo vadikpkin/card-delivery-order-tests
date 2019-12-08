55589d495fc01a95fa376ea9452ab748977f0d8a
Vadim Pkin <pkinchek@gmail.com>
2019-12-08 21:56:12 +0300

    happypath test added

--- src/test/java/CardDeliveryOrderTests.java
+++ src/test/java/CardDeliveryOrderTests.java
@@ -1,12 +1,31 @@
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selectors;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import java.time.LocalDate;
+
 import static com.codeborne.selenide.Selenide.*;
 
-public class CardDeliveryOrderTests {
-    static final String url = "http://localhost:9999/";
+class CardDeliveryOrderTests {
+    private static final String url = "http://localhost:9999/";
+    private static final LocalDate dayOfMeeting = LocalDate.now().plusDays(4);
+    private static final String dayOfMeetingString = dayOfMeeting.getDayOfMonth()
+            + Integer.toString(dayOfMeeting.getMonthValue()) + dayOfMeeting.getYear();
 
-    @Test
-    void shouldOpenURL(){
+    @CsvFileSource(resources = "/cardDeliveryOrderTestSubmitSources", numLinesToSkip = 1)
+    @ParameterizedTest
+    void shouldSubmitRequest(String town, String name, String tel){
         open(url);
+        $("[placeholder = 'Город']").setValue(town);
+        SelenideElement dateWebElement = $("[placeholder = 'Дата встречи']");
+        dateWebElement.setValue("\b\b\b\b\b\b\b\b\b\b");
+        dateWebElement.setValue(dayOfMeetingString);
+        $("[name='name']").setValue(name);
+        $("[name='phone']").setValue(tel);
+        $(".checkbox__box").click();
+        $(".button").click();
+        $(new Selectors.ByText("Забронировать")).click();
+        $("[data-test-id='notification']").waitUntil(Condition.visible, 15000);
     }
-
 }
